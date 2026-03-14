656b4d901da4516a9ca3267103918008fd5919ca
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-14 22:32:41 +0530

    Create 1486. XOR_Operation_in_an_Array.py

---
+++ Python/1486. XOR_Operation_in_an_Array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def xorOperation(self, n, start):
+        for i in range(n):
+            a=start+2*i
+        
+        for i in range(1,n):
+            s^=nums[i]
+        return s
