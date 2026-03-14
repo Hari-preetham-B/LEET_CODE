c7a0a77b93c31ceab7b325027fcf9276e42165e9
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-14 21:14:47 +0530

    Create 56. Merge_Intervals.py

---
+++ Python/56. Merge_Intervals.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def merge(self, intervals):
+        intervals = sorted(intervals, key = lambda x:x[0])
+        final = []
+        start,end = intervals[0]
+        for s,e in intervals[1:]:
+            if s<=end:
+                end = max(end,e)
+            else:
+                final.append([start,end])
+                start,end = s,e
+        final.append([start,end])
+        return final
