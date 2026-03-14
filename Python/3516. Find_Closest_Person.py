7519ea5f76359d6ef4ad6bbfa0647cdc7d28585a
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-14 23:08:44 +0530

    Create 3516. Find_Closest_Person.py

---
+++ Python/3516. Find_Closest_Person.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        a = abs(x-z)
+        b = abs(y-z)
+        if a<b:
+            return 1
+        elif a>b:
+            return 2
+        else:
+            return 0
+        
