341385b0e5bd14590e37857726dfd442017b983b
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-14 22:42:41 +0530

    Create 2011. Final_Value_of_Variable_After_Performing_Operations.py

---
+++ Python/2011. Final_Value_of_Variable_After_Performing_Operations.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        x=0
+        for i in operations:
+            if i== "X++":
+                x+=1
+            elif i=="++X":
+                x+=1;
+            elif i=="X--":
+                x-=1;
+            elif i=="--X":
+                x-=1
+        return x
