e637176b4e36c3a6e66ef907c775fd2b733b42c7
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-14 22:28:33 +0530

    Create 1431. Kids_With_the_Greatest_Number_of_Candies.py

---
+++ Python/1431. Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        result=[]
+        m=max(candies)
+        for i in candies:
+            if i+extraCandies>=m:
+                result.append(True)
+            else:
+                result.append(False)
+        return result
+        
