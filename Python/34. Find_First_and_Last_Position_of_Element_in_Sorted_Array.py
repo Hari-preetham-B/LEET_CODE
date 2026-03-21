a4608cc76dbb98894812a5280f47bb64d507b12f
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-03-21 22:34:30 +0530

    Update 34. Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- Python/34. Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ Python/34. Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -1,11 +1,43 @@
 class Solution(object):
     def searchRange(self, nums, target):
-        i=nums.count(target)
-        if i==0:
-            return [-1,-1]
-        if i==1:
-            j=nums.index(target)
-            return [j,j]
-        if i>1:
-            j=nums.index(target)
-            return [j,j+i-1]
+        
+        def findFirst(nums, target):
+            l, r = 0, len(nums) - 1
+            ans = -1
+            
+            while l <= r:
+                mid = (l + r) // 2
+                
+                if nums[mid] == target:
+                    ans = mid
+                    r = mid - 1   # keep searching LEFT
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            return ans
+        
+        
+        def findLast(nums, target):
+            l, r = 0, len(nums) - 1
+            ans = -1
+            
+            while l <= r:
+                mid = (l + r) // 2
+                
+                if nums[mid] == target:
+                    ans = mid
+                    l = mid + 1   # keep searching RIGHT
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            return ans
+        
+        
+        first = findFirst(nums, target)
+        last = findLast(nums, target)
+        
+        return [first, last]
