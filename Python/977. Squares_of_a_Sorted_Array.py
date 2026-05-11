752491af7b7ed9c1026e83e02b58e13e0a5e182c
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-05-11 10:57:56 +0530

    Update 977. Squares_of_a_Sorted_Array.py

--- Python/977. Squares_of_a_Sorted_Array.py
+++ Python/977. Squares_of_a_Sorted_Array.py
@@ -6,3 +6,23 @@ class Solution(object):
         nums.sort()
         return nums
         
+# or
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        l=0
+        r=len(nums)-1
+        pos=r
+        result=[0]*(pos+1)
+        while l<=r:
+            ls=nums[l]**2
+            rs=nums[r]**2
+            if ls<rs:
+                result[pos]=rs
+                r-=1
+                pos-=1
+            else:
+                result[pos]=ls
+                l+=1
+                pos-=1
+        return result
