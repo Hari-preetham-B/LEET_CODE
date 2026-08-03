9d1a35edcad10b4df4ff67246b8eaaf772f9f4d8
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-08-03 20:12:15 +0530

    Implement regular expression matching with dynamic programming

---
+++ Python/10. Regular Expression Matching.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        m, n = len(s), len(p)
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = True
+        for j in range(1, n + 1):
+            if p[j - 1] == '*' and j >= 2:
+                dp[0][j] = dp[0][j - 2]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    zero_case = dp[i][j - 2] if j >= 2 else False
+                    one_or_more_case = False
+                    if j >= 2 and (p[j - 2] == '.' or p[j - 2] == s[i - 1]):
+                        one_or_more_case = dp[i - 1][j]
+                    dp[i][j] = zero_case or one_or_more_case
+                else:
+                    dp[i][j] = False
+        return dp[m][n]
