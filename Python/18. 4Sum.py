68fc3b9929d8a348b34e6577b5d65309adf0e691
Bade Hari Preetham <haripreetham.1111@gmail.com>
2026-08-16 15:49:26 +0530

    Refactor 4Sum function to use range instead of xrange

--- Python/18. 4Sum.py
+++ Python/18. 4Sum.py
@@ -1,54 +1,38 @@
 class Solution(object):
     def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
         nums.sort()
         n = len(nums)
         result = []
 
-        for i in xrange(n - 3):
+        for i in range(n - 3):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
-            # Minimum possible sum
-            if nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target:
-                break
-
-            # Maximum possible sum
-            if nums[i] + nums[n - 3] + nums[n - 2] + nums[n - 1] < target:
-                continue
-
-            for j in xrange(i + 1, n - 2):
+            for j in range(i + 1, n - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
 
-                # Minimum possible sum for this j
-                if nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target:
-                    break
-
-                # Maximum possible sum for this j
-                if nums[i] + nums[j] + nums[n - 2] + nums[n - 1] < target:
-                    continue
-
-                left = j + 1
-                right = n - 1
+                left, right = j + 1, n - 1
 
                 while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                    current_sum = nums[i] + nums[j] + nums[left] + nums[right]
 
-                    if total == target:
-                        result.append([
-                            nums[i], nums[j], nums[left], nums[right]
-                        ])
+                    if current_sum == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
 
                         left += 1
                         right -= 1
-
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-
-                    elif total < target:
+                    elif current_sum < target:
                         left += 1
                     else:
                         right -= 1
